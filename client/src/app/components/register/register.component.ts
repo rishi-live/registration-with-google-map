ee90473d69fcfe668e71200d68bb7561b3eafd54
rishi-live <rishi.uemk9@gmail.com>
2021-10-10 23:21:12 +0530

    register form added

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from 'src/app/models/user';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  // name: string = "Rishi";
+  user: User = new User();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  register() {
+    console.log("register user",);
+    console.log({user:this.user});
+  }
+  onAddressType(value: any) {
+    console.log(value)
+  }
 }
---
+++ client/src/app/models/user.ts
@@ -0,0 +1,10 @@
+import { Address } from "./address";
+
+export class User {
+    firstName: string
+    lastName: string
+    email: string
+    mobile: string
+    password: string
+    address: Address
+}
